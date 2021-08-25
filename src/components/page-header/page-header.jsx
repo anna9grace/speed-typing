d71baa03d4047c3319d219ddec60770df8bbc2c9
anna9grace <anna9grace@gmail.com>
2021-08-25 21:53:18 +0300

    add styles to header component

--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-// import classNames from 'classnames';
+import classNames from 'classnames';
+import styles from './page-header.module.scss';
 
 function PageHeader(props) {
 
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <header className={styles.container}>
+      <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Тренажер быстрой печати</a>
           <div>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" href="/">Выйти</a>
-              </li>
-              <li className="nav-item">
+              <li className={styles.navlink}>
                 <a className="nav-link" href="/">Статистика Юзер</a>
+              </li>
+              <li className={styles.navlink}>
+                <a className="nav-link" href="/">Выйти</a>
               </li>
             </ul>
           </div>
