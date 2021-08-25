c6d1ad6e345f95779716312db9fb1b5bf53e8f5f
anna9grace <anna9grace@gmail.com>
2021-08-25 21:38:13 +0300

    add style to message component

--- src/components/message/message.jsx
+++ src/components/message/message.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import styles from './message.module.scss';
 
-function Message({className, children, title = '', ...attrs}) {
-
-  const classnames = classNames(
-    'row',
-    'justify-content-center',
-    className,
-  );
-
+function Message({children, ...props}) {
   return (
-    <div className={classnames} {...attrs} style={{minHeight: '100px', paddingTop: '25px', paddingBottom: '25px'}}>
-      <div className="col-6 bg-light" style={{display: 'none'}}>
+    <div className={classNames('row', 'justify-content-center', styles.container)}>
+      <div className={classNames('col-6', styles.wrapper)}>
         {children}
       </div>
     </div>
@@ -22,7 +16,6 @@ function Message({className, children, title = '', ...attrs}) {
 Message.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  title: PropTypes.string,
 };
 
 export default Message;
