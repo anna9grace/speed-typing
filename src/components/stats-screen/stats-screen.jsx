8931d1173263cefe512f55299c19f23abbfa0528
anna9grace <anna9grace@gmail.com>
2021-08-30 13:41:57 +0300

    realize displaying user's results

--- src/components/stats-screen/stats-screen.jsx
+++ src/components/stats-screen/stats-screen.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router';
+import classNames from 'classnames';
 
+import styles from './stats-screen.module.scss';
+
+import PageHeader from '../page-header/page-header';
 import { getResults, getUserInfo } from '../../store/selectors';
 import { fetchResults } from '../../store/api-actions';
+import { AppRoutes } from '../../constants';
 
 function StatsScreen () {
   const results = useSelector(getResults);
@@ -10,20 +16,34 @@ function StatsScreen () {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchResults(user));
-  }, [results]);
+    user.id && dispatch(fetchResults(user));
+  }, [user]);
+
+  if (!user.id) {
+    return <Redirect to={AppRoutes.ROOT} />;
+  }
 
   return (
     <React.Fragment>
-      <h1>Результаты пользователя <span>{user.name}</span></h1>
-      <ol>
-        {results.map((res) => (
-          <li key={res.id}>
-            <p>Скорость: <span>{res.speed}</span></p>
-            <p>Точность: <span>{res.precision}</span></p>
-          </li>
-        ))}
-      </ol>
+      <PageHeader />
+      <main className={classNames('row', 'align-items-center')}>
+        <div className={classNames('row', 'justify-content-center', 'align-items-center')}>
+          <h1 className={styles.title}>Результаты пользователя <span>{user.name}</span></h1>
+          <div className={classNames('col-4', 'row', 'justify-content-center', 'align-items-center', styles.wrapper)}>
+
+            <ol >
+              {results.map((res, index) => (
+                <li key={`${res.id}${res.speed}`}>
+                  <p>Скорость: <span className={styles.result}>{res.speed}</span></p>
+                  <p>Точность: <span className={styles.result}>{res.precision}</span></p>
+                  {index !== results.length - 1 && <hr/>}
+                </li>
+              ))}
+            </ol>
+
+          </div>
+        </div>
+      </main>
     </React.Fragment>
   );
 }
